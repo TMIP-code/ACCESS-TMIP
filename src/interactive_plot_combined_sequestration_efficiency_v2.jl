87004efe4cae0395d047b0d8700eecbdce785d2a
Benoit Pasquier <briochemc@gmail.com>
2025-01-09 14:25:58 +1100

    Finished toy test for 1d column calgtilde

--- src/interactive_plot_combined_sequestration_efficiency_v2.jl
+++ src/interactive_plot_combined_sequestration_efficiency_v2.jl
@@ -1,95 +1,95 @@
 
-# using Pkg
-# Pkg.activate(".")
-# Pkg.instantiate()
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
 
 
-# ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
-# using OceanTransportMatrixBuilder
-# using NetCDF
-# using YAXArrays
-# using DataFrames
-# using DimensionalData
-# using SparseArrays
-# using LinearAlgebra
-# using Unitful
-# using Unitful: s, yr, d
-# using Statistics
-# using Format
-# using Dates
-# using FileIO
-# using ProgressMeter
-# using GLMakie
-# using GeoMakie
-# using OceanBasins
-# using NaNStatistics
-
-
-
-# include("plotting_functions.jl")
-
-
-# # script options
-# @show model = "ACCESS-ESM1-5"
-# if isempty(ARGS)
-#     member = "r1i1p1f1"
-#     # experiment = "historical"
-#     # time_window = "Jan1850-Dec1859"
-#     # time_window = "Jan1990-Dec1999"
-#     experiment = "ssp370"
-#     time_window = "Jan2030-Dec2039"
-#     # time_window = "Jan2090-Dec2099"
-#     WRITEDATA = "true"
-# else
-#     experiment, member, time_window, finalmonth, WRITEDATA = ARGS
-# end
-# WRITEDATA = parse(Bool, WRITEDATA)
-# @show experiment
-# @show member
-# @show time_window
-
-
-# # Load areacello and volcello for grid geometry
-# fixedvarsinputdir = "/Users/benoitpasquier/Data/TMIP/data/$model"
-# volcello_ds = open_dataset(joinpath(fixedvarsinputdir, "volcello.nc"))
-# areacello_ds = open_dataset(joinpath(fixedvarsinputdir, "areacello.nc"))
-
-# # Gadi directory for input files
-# lumpby = "month"
-# inputdir = "/Users/benoitpasquier/Data/TMIP/data/$model/$experiment/$member/$(time_window)"
-# cycloinputdir = joinpath(inputdir, "cyclo$lumpby")
+ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+using OceanTransportMatrixBuilder
+using NetCDF
+using YAXArrays
+using DataFrames
+using DimensionalData
+using SparseArrays
+using LinearAlgebra
+using Unitful
+using Unitful: s, yr, d
+using Statistics
+using Format
+using Dates
+using FileIO
+using ProgressMeter
+using GLMakie
+using GeoMakie
+using OceanBasins
+using NaNStatistics
 
 
 
-# # Load fixed variables in memory
-# areacello = readcubedata(areacello_ds.areacello)
-# volcello = readcubedata(volcello_ds.volcello)
-# lon = readcubedata(volcello_ds.lon)
-# lat = readcubedata(volcello_ds.lat)
-# lev = volcello_ds.lev
-# # Identify the vertices keys (vary across CMIPs / models)
-# volcello_keys = propertynames(volcello_ds)
-# lon_vertices_key = volcello_keys[findfirst(x -> occursin("lon", x) & occursin("vert", x), string.(volcello_keys))]
-# lat_vertices_key = volcello_keys[findfirst(x -> occursin("lat", x) & occursin("vert", x), string.(volcello_keys))]
-# lon_vertices = readcubedata(getproperty(volcello_ds, lon_vertices_key))
-# lat_vertices = readcubedata(getproperty(volcello_ds, lat_vertices_key))
+include("plotting_functions.jl")
 
-# # Make makegridmetrics
-# gridmetrics = makegridmetrics(; areacello, volcello, lon, lat, lev, lon_vertices, lat_vertices)
-# (; lon_vertices, lat_vertices, v3D) = gridmetrics
-# lon2 = mod.(gridmetrics.lon .- 80, 360) .+ 80
 
-# # Make indices
-# indices = makeindices(v3D)
-# (; N, wet3D) = indices
+# script options
+@show model = "ACCESS-ESM1-5"
+if isempty(ARGS)
+    member = "r1i1p1f1"
+    # experiment = "historical"
+    # time_window = "Jan1850-Dec1859"
+    # time_window = "Jan1990-Dec1999"
+    experiment = "ssp370"
+    time_window = "Jan2030-Dec2039"
+    # time_window = "Jan2090-Dec2099"
+    WRITEDATA = "true"
+else
+    experiment, member, time_window, finalmonth, WRITEDATA = ARGS
+end
+WRITEDATA = parse(Bool, WRITEDATA)
+@show experiment
+@show member
+@show time_window
 
-# # Load netCDF file
-# inputfile = joinpath(inputdir, "calE.nc")
-# @info "Loading mean sequestration efficiency as netCDF file:\n  $(inputfile)"
-# ds = open_dataset(inputfile)
-# ℰ = readcubedata(ds.calE).data
 
-# years = ds.Ti |> Array
+# Load areacello and volcello for grid geometry
+fixedvarsinputdir = "/Users/benoitpasquier/Data/TMIP/data/$model"
+volcello_ds = open_dataset(joinpath(fixedvarsinputdir, "volcello.nc"))
+areacello_ds = open_dataset(joinpath(fixedvarsinputdir, "areacello.nc"))
+
+# Gadi directory for input files
+lumpby = "month"
+inputdir = "/Users/benoitpasquier/Data/TMIP/data/$model/$experiment/$member/$(time_window)"
+cycloinputdir = joinpath(inputdir, "cyclo$lumpby")
+
+
+
+# Load fixed variables in memory
+areacello = readcubedata(areacello_ds.areacello)
+volcello = readcubedata(volcello_ds.volcello)
+lon = readcubedata(volcello_ds.lon)
+lat = readcubedata(volcello_ds.lat)
+lev = volcello_ds.lev
+# Identify the vertices keys (vary across CMIPs / models)
+volcello_keys = propertynames(volcello_ds)
+lon_vertices_key = volcello_keys[findfirst(x -> occursin("lon", x) & occursin("vert", x), string.(volcello_keys))]
+lat_vertices_key = volcello_keys[findfirst(x -> occursin("lat", x) & occursin("vert", x), string.(volcello_keys))]
+lon_vertices = readcubedata(getproperty(volcello_ds, lon_vertices_key))
+lat_vertices = readcubedata(getproperty(volcello_ds, lat_vertices_key))
+
+# Make makegridmetrics
+gridmetrics = makegridmetrics(; areacello, volcello, lon, lat, lev, lon_vertices, lat_vertices)
+(; lon_vertices, lat_vertices, v3D) = gridmetrics
+lon2 = mod.(gridmetrics.lon .- 80, 360) .+ 80
+
+# Make indices
+indices = makeindices(v3D)
+(; N, wet3D) = indices
+
+# Load netCDF file
+inputfile = joinpath(inputdir, "calE.nc")
+@info "Loading mean sequestration efficiency as netCDF file:\n  $(inputfile)"
+ds = open_dataset(inputfile)
+ℰ = readcubedata(ds.calE).data
+
+years = ds.Ti |> Array
 
 
 
