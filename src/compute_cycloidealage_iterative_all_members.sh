c3c16ed92ec8c20492f44c0d6e5a7392c661b2f9
Benoit Pasquier <briochemc@gmail.com>
2025-02-13 16:06:30 +1100

    compute ensemble of low kappa ages

---
+++ src/compute_cycloidealage_iterative_all_members.sh
@@ -0,0 +1,5 @@
+for member in r{2..40}i1p1f1; do
+    sed "s/member_placeholder/$member/g" src/compute_cycloidealage_iterative_varied_members.sh > src/tmp.sh
+    qsub src/tmp.sh
+    rm src/tmp.sh
+done
---
+++ src/compute_cycloidealage_iterative_varied_members.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#PBS -P xv83
+#PBS -N age_member_placeholder
+#PBS -l ncpus=24
+#PBS -q hugemem
+#PBS -l mem=500GB
+#PBS -l jobfs=4GB
+#PBS -l walltime=1:00:00
+#PBS -l storage=scratch/gh0+scratch/xv83
+#PBS -l wd
+#PBS -o output/PBS/
+#PBS -j oe
+
+echo "Going into ACCESS-TMIP"
+cd ~/Projects/TMIP/ACCESS-TMIP
+
+experiment=historical
+# time_window=Jan1990-Dec1999
+time_window=Jan1850-Dec1859
+# experiment=ssp370
+# time_window=Jan2030-Dec2039
+# time_window=Jan2090-Dec2099
+
+member=member_placeholder
+
+echo "Computing age for $experiment $member $time_window"
+julia src/compute_cycloidealage_iterative_varied_members.jl $experiment $member $time_window &> output/$PBS_JOBID.compute_cycloidealage_iterative_varied_members.$experiment.$member.$time_window.out
+
+
