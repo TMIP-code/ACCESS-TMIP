f49da79ebc6a6e7262b81f326355c5f73e1b607c
Benoit Pasquier <briochemc@gmail.com>
2025-02-13 11:08:12 +1100

    compute age with very low kVML

--- src/compute_cycloidealage_iterative_all_diffusivities.sh
+++ src/compute_cycloidealage_iterative_all_diffusivities.sh
@@ -1,5 +1,5 @@
 # for kVdeep in 1e-7 3e-7 1e-6 3e-6 1e-5 3e-5 1e-4 3e-4; do
-for kVML in 0.001 0.01 0.1 1; do
+for kVML in 1e-7 1e-6 1e-5 1e-4; do
     # for kH in 5 15; do
     # sed "s/kVdeep_placeholder/$kVdeep/g" src/compute_cycloidealage_iterative_varied_diffusivities.sh > src/tmp.sh
     # sed "s/kH_placeholder/$kH/g" src/tmp.sh > src/tmp2.sh
