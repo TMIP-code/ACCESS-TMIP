92d7b7d7ea7ec0af8237a0e8d5c5e060a61cd4a4
Benoit Pasquier <briochemc@gmail.com>
2025-02-13 11:04:41 +1100

    fix kappa VML age sweep

--- src/compute_cycloidealage_iterative_all_diffusivities.sh
+++ src/compute_cycloidealage_iterative_all_diffusivities.sh
@@ -3,9 +3,9 @@ for kVML in 0.001 0.01 0.1 1; do
     # for kH in 5 15; do
     # sed "s/kVdeep_placeholder/$kVdeep/g" src/compute_cycloidealage_iterative_varied_diffusivities.sh > src/tmp.sh
     # sed "s/kH_placeholder/$kH/g" src/tmp.sh > src/tmp2.sh
-    sed "s/kVML_placeholder/$kVML/g" src/compute_cycloidealage_iterative_varied_diffusivities.sh > src/tmp.sh
+    sed "s/kVML_placeholder/$kVML/g" src/compute_cycloidealage_iterative_varied_diffusivities.sh > src/tmp2.sh
     qsub src/tmp2.sh
-    rm src/tmp.sh
+    # rm src/tmp.sh
     rm src/tmp2.sh
     # done
 done
