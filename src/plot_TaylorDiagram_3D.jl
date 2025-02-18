d052e32074fa26435ca28ce1e606f6d9ca9bb993
Benoit Pasquier <briochemc@gmail.com>
2025-02-18 11:12:49 +1100

    Add heatmaps of Taylor diagram scores

--- src/plot_TaylorDiagram_3D.jl
+++ src/plot_TaylorDiagram_3D.jl
@@ -92,11 +92,11 @@
 # end
 
 
-# # @info "Loading Anderson Acceleration age"
-# # # Anderson Accelerated age
-# # AAfile = "/scratch/xv83/bp3051/access-esm/archive/andersonacceleration_test-n10-5415f621/age_output/ocean_age.res_0035.nc"
-# # obs_ds = open_dataset(AAfile)
-# # obs_data = obs_ds.age_global[Time=1].data[wet3D]
+# @info "Loading Anderson Acceleration age"
+# # Anderson Accelerated age
+# AAfile = "/scratch/xv83/bp3051/access-esm/archive/andersonacceleration_test-n10-5415f621/age_output/ocean_age.res_0035.nc"
+# obs_ds = open_dataset(AAfile)
+# obs_data = obs_ds.age_global[Time=1].data[wet3D]
 
 
 
@@ -137,141 +137,244 @@
 # # Calculate the Taylor diagram values
 # w = weights(v3D[wet3D])
 # TDvals = [taylordiagramvalues(data, obs_data, w) for data in model_data]
-σr = TDvals[1].σr
-σmax = 1.2TDvals[1].σr
+# σr = TDvals[1].σr
+# σmax = 1.2TDvals[1].σr
 
 
 
 
-# Do the actual plotting now
-# First, construct the figure and a polar axis on the first quadrant
-fig = Figure(size=(600, 600))
+# # Do the actual plotting now
+# # First, construct the figure and a polar axis on the first quadrant
+# fig = Figure(size=(600, 600))
 
-# Corrticks for Taylor diagram
-# corrticks = [-1; -0.99; -0.95; -0.9:0.1:-0.7; -0.6:0.2:0.6; 0.7:0.1:0.9; 0.95; 0.99; 1.0]
-corrticks = [0:0.2:0.6; 0.7:0.1:0.9; 0.95; 0.99; 1.0]
-function myformat(corrtick)
-    isinteger(corrtick) && (corrtick = Int(corrtick))
-    str = string(corrtick)
-    return replace(str, "-" => "−")
+# # Corrticks for Taylor diagram
+# # corrticks = [-1; -0.99; -0.95; -0.9:0.1:-0.7; -0.6:0.2:0.6; 0.7:0.1:0.9; 0.95; 0.99; 1.0]
+# corrticks = [0:0.2:0.6; 0.7:0.1:0.9; 0.95; 0.99; 1.0]
+# function myformat(corrtick)
+#     isinteger(corrtick) && (corrtick = Int(corrtick))
+#     str = string(corrtick)
+#     return replace(str, "-" => "−")
+# end
+
+
+# ax = PolarAxis(fig[1, 1];
+#     thetalimits = (0, π/2), # first quadrant only
+#     thetagridcolor = (:black, 0.5),
+#     thetagridstyle = :dot,
+#     thetaticks = (acos.(corrticks), myformat.(corrticks)),
+#     # thetaminorticks,
+#     rlimits = (0, σmax),
+#     rgridcolor = cgrad(:Archambault, categorical = true)[1],
+#     rticklabelcolor = cgrad(:Archambault, categorical = true)[1],
+#     rgridstyle = :dash,
+#     rticks = 0:σr:σmax,
+# )
+
+# # Create isolines of root centered mean squared difference (E′) and labels
+# levels = (0.5:0.5:4) .* σr
+# rgrid = (0:0.01:1) .* σmax
+# θgrid = (0:0.01:π)
+# E′fun(σf, σr, R) = sqrt(σf^2 + σr^2 - 2 * σf * σr * R)
+# # E′grid = [sqrt(r^2 + σr^2 - 2 * σr * r * cos(θ)) for θ in θgrid, r in rgrid]
+# E′grid = [E′fun(r, σr, cos(θ)) for θ in θgrid, r in rgrid]
+# # labelformatter(E′s) = map(E′ -> rich("$(E′/σr)", rich(" σ", subscript("ref"))), E′s)
+# labelformatter(E′s) = map(E′ -> "$(format(round(10E′/σr)/10, stripzeros = true))σᵣ", E′s)
+# contour!(ax, θgrid, rgrid, E′grid;
+#     levels,
+#     labels = true,
+#     labelformatter,
+#     color = cgrad(:Archambault, categorical = true)[3]
+# )
+
+# # skill score isolines
+# R₀ = 0.9940801590730742 # maximum correlation obtainable from ensemble
+# S(σf, σr, R) = 4 * (1 + R) / ((σf/σr + σr/σf)^2 * (1 + R₀))
+# # S(σf, σr, R) = 4 * (1 + R)^4 / ((σf/σr + σr/σf)^2 * (1 + R₀)^4)
+# Sgrid = [S(r, σr, cos(θ)) for θ in θgrid, r in rgrid]
+# Slevels = [0:0.1:0.9; 0.95; 0.99]
+# contour!(ax, θgrid, rgrid, Sgrid;
+#     levels = Slevels,
+#     labels = true,
+#     color = cgrad(:Archambault, categorical = true)[4]
+# )
+
+# # Now, plot the actual data
+# σfs = [vals.σf for vals in TDvals]
+# Rs = [vals.R for vals in TDvals]
+# E′s = [vals.E′ for vals in TDvals]
+# Ēs = [vals.Ē for vals in TDvals]
+
+# # Transform data to Cartesian space?
+# "A transformation function that goes from correlation and standard deviation to the Taylor plot's Cartesian space."
+# xy_from_R_and_σ(R, σ) = Point2(σ * R, sqrt(σ^2 - (σ * R)^2))
+# Ps = xy_from_R_and_σ.(Rs, σfs)
+# # Above I used R = 1 and σr for the reference point
+# # Plot all matrix ages. No, too many, I'll just plot those of the edges of the 3D cube
+# transformation = Transformation(ax.scene.transformation; transform_func = identity)
+# # Ps[2:end-1, 2:end-1, :] .= NaN
+# cube_lines = reduce(vcat, reduce(vcat, [v; [Point2([NaN, NaN])]] for v in eachslice(Ps; dims)) for dims in ((1,2), (1,3), (2,3)))
+# cube_lines = [
+#     Ps[1,1,:]; [Point2(NaN, NaN)];
+#     Ps[1,end,:]; [Point2(NaN, NaN)];
+#     Ps[end,1,:]; [Point2(NaN, NaN)];
+#     Ps[end,end,:]; [Point2(NaN, NaN)];
+#     Ps[1,:,1]; [Point2(NaN, NaN)];
+#     Ps[1,:,end]; [Point2(NaN, NaN)];
+#     Ps[end,:,1]; [Point2(NaN, NaN)];
+#     Ps[end,:,end]; [Point2(NaN, NaN)];
+#     Ps[:,1,1]; [Point2(NaN, NaN)];
+#     Ps[:,1,end]; [Point2(NaN, NaN)];
+#     Ps[:,end,1]; [Point2(NaN, NaN)];
+#     Ps[:,end,end]; [Point2(NaN, NaN)];
+# ]
+
+# text!(ax, Ps[1,1,1]; text = "min", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
+# text!(ax, Ps[end,1,1]; text = "max Vdeep", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
+# text!(ax, Ps[1,end,1]; text = "max VML", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
+# text!(ax, Ps[1,1,end]; text = "max H", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
+# text!(ax, Ps[end,end,end]; text = "max", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
+# scatter!(ax, Ps[:];
+#     color = [TDval.Ē for TDval in TDvals][:],
+#     colorrange = (-200, 200),
+#     colormap = cgrad(:tableau_red_blue, rev = true),
+#     # marker = :cross,
+#     markersize = 5,
+#     strokewidth = 1,
+#     strokecolor = :black,
+#     transformation = Transformation(ax.scene.transformation; transform_func = identity)
+# )
+# sc = scatter!(ax, Ps[:];
+#     color = [TDval.Ē for TDval in TDvals][:],
+#     colorrange = (-200, 200),
+#     colormap = cgrad(:tableau_red_blue, rev = true),
+#     # marker = :cross,
+#     markersize = 5,
+#     transformation = Transformation(ax.scene.transformation; transform_func = identity)
+# )
+# scatterlines!(ax, cube_lines;
+#     color = (:black, 0.1),
+#     markersize = 1,
+#     markercolor = :black,
+#     linewidth = 1,
+#     transformation,
+# )
+
+# # Plot reference (AA age)
+# scatter!(ax, Point2(xy_from_R_and_σ(1, σr));
+#     color = :black,
+#     transformation,
+# )
+
+# cb = Colorbar(fig[2, 1], sc;
+#     vertical = false,
+#     flipaxis = false,
+#     tellheight = true,
+#     # ticks = (, cbarticklabelformat.(levels)),
+#     label = rich("bias (years)"),
+# )
+# cb.width = Relative(0.5)
+
+# outputfile = joinpath(outputdir, "Taylor_diagram_3D.pdf")
+# @info "Saving image file:\n  $(outputfile)"
+# save(outputfile, fig)
+
+
+
+
+fig = Figure(size=(1200, 1200))
+
+options = (
+    xlabel = "κVML",
+    xticks = (1:length(κVMLs), format.(κVMLs, conversion = "g")),
+    ylabel = "κH",
+    yticks = (1:length(κHs), format.(κHs, conversion = "e")),
+)
+
+colorscale = ReversibleScale(x -> 1 - 2acos(x) / π, x -> cos(π/2 * (1 - x)), limits = (0.001, 0.999))
+
+
+
+for irow in 1:5
+    # Correlation
+    ax = Axis(fig[irow, 1]; options...)
+    kwargs = (
+        colorrange = (0.5, 1),
+        colormap = :viridis,
+        colorscale,
+    )
+    hm1 = heatmap!(ax, Rs[irow, :, :]; kwargs...)
+    (irow ≠ 5) && hidexdecorations!(ax)
+
+    # STD - STDref
+    ax = Axis(fig[irow, 2]; options...)
+    kwargs = (
+        colorrange = (-σr/2, σr/2),
+        colormap = cgrad(:RdBu, rev = true),
+    )
+    hm2 = heatmap!(ax, σfs[irow, :, :] .- σr; kwargs...)
+    hideydecorations!(ax)
+    (irow ≠ 5) && hidexdecorations!(ax)
+
+
+    # RMS
+    ax = Axis(fig[irow, 3]; options...)
+    kwargs = (
+        colorrange = (0, σr/2),
+        colormap = :plasma,
+    )
+    hm3 = heatmap!(ax, E′s[irow, :, :]; kwargs...)
+    hideydecorations!(ax)
+    (irow ≠ 5) && hidexdecorations!(ax)
+
+    # bias
+    ax = Axis(fig[irow, 4]; options...)
+    kwargs = (
+        colorrange = (-200, 200),
+        colormap = cgrad(:tableau_red_blue, rev = true),
+    )
+    hm4 = heatmap!(ax, Ēs[irow, :, :]; kwargs...)
+    hideydecorations!(ax)
+    (irow ≠ 5) && hidexdecorations!(ax)
+
+    # score
+    ax = Axis(fig[irow, 5]; options...)
+    kwargs = (
+        colorrange = (0.9, 0.97),
+        colormap = :cividis,
+        colorscale,
+    )
+    hm5 = heatmap!(ax, S.(σfs, σr, Rs)[irow, :, :]; kwargs...)
+    hideydecorations!(ax)
+    (irow ≠ 5) && hidexdecorations!(ax)
+
+    Label(fig[irow, 0]; text = "κVDeep = " * format.(κVdeeps[irow], conversion = "e"), rotation = π/2, tellheight = false)
+
+    if irow == 5
+        cbopt = (
+            vertical = false,
+            flipaxis = false,
+            tellheight = true,
+            width = Relative(0.85),
+        )
+        cb = Colorbar(fig[6, 1], hm1; cbopt..., label = rich("correlation"), ticks = [0, 0.5, 0.8, 0.95, 1])
+        cb = Colorbar(fig[6, 2], hm2; cbopt..., label = rich("STD - STDref"))
+        cb = Colorbar(fig[6, 3], hm3; cbopt..., label = rich("RMS"))
+        cb = Colorbar(fig[6, 4], hm4; cbopt..., label = rich("bias"))
+        cb = Colorbar(fig[6, 5], hm5; cbopt..., label = rich("skill score"), ticks = [0.9, 0.95, 0.97])
+    end
+
 end
 
-ax = PolarAxis(fig[1, 1];
-    thetalimits = (0, π/2), # first quadrant only
-    thetagridcolor = (:black, 0.5),
-    thetagridstyle = :dot,
-    thetaticks = (acos.(corrticks), myformat.(corrticks)),
-    # thetaminorticks,
-    rlimits = (0, σmax),
-    rgridcolor = cgrad(:Archambault, categorical = true)[1],
-    rticklabelcolor = cgrad(:Archambault, categorical = true)[1],
-    rgridstyle = :dash,
-    rticks = 0:σr:σmax,
-)
 
-# Create isolines of root centered mean squared difference (E′) and labels
-levels = (0.5:0.5:4) .* σr
-rgrid = (0:0.01:1) .* σmax
-θgrid = (0:0.01:π)
-E′fun(σf, σr, R) = sqrt(σf^2 + σr^2 - 2 * σf * σr * R)
-# E′grid = [sqrt(r^2 + σr^2 - 2 * σr * r * cos(θ)) for θ in θgrid, r in rgrid]
-E′grid = [E′fun(r, σr, cos(θ)) for θ in θgrid, r in rgrid]
-# labelformatter(E′s) = map(E′ -> rich("$(E′/σr)", rich(" σ", subscript("ref"))), E′s)
-labelformatter(E′s) = map(E′ -> "$(format(round(10E′/σr)/10, stripzeros = true))σᵣ", E′s)
-contour!(ax, θgrid, rgrid, E′grid;
-    levels,
-    labels = true,
-    labelformatter,
-    color = cgrad(:Archambault, categorical = true)[3]
-)
 
-# skill score isolines
-R₀ = 0.9940801590730742 # maximum correlation obtainable from ensemble
-S(σf, σr, R) = 4 * (1 + R) / ((σf/σr + σr/σf)^2 * (1 + R₀))
-# S(σf, σr, R) = 4 * (1 + R)^4 / ((σf/σr + σr/σf)^2 * (1 + R₀)^4)
-Sgrid = [S(r, σr, cos(θ)) for θ in θgrid, r in rgrid]
-Slevels = [0:0.1:0.9; 0.95; 0.99]
-contour!(ax, θgrid, rgrid, Sgrid;
-    levels = Slevels,
-    labels = true,
-    color = cgrad(:Archambault, categorical = true)[4]
-)
+Label(fig[0,1]; text = "correlation", tellwidth = false)
+Label(fig[0,2]; text = "STD - STDref", tellwidth = false)
+Label(fig[0,3]; text = "RMS", tellwidth = false)
+Label(fig[0,4]; text = "bias", tellwidth = false)
+Label(fig[0,5]; text = "skill score", tellwidth = false)
 
-# Now, plot the actual data
-σfs = [vals.σf for vals in TDvals]
-Rs = [vals.R for vals in TDvals]
-
-# Transform data to Cartesian space?
-"A transformation function that goes from correlation and standard deviation to the Taylor plot's Cartesian space."
-xy_from_R_and_σ(R, σ) = Point2(σ * R, sqrt(σ^2 - (σ * R)^2))
-Ps = xy_from_R_and_σ.(Rs, σfs)
-# Above I used R = 1 and σr for the reference point
-# Plot all matrix ages. No, too many, I'll just plot those of the edges of the 3D cube
-transformation = Transformation(ax.scene.transformation; transform_func = identity)
-# Ps[2:end-1, 2:end-1, :] .= NaN
-cube_lines = reduce(vcat, reduce(vcat, [v; [Point2([NaN, NaN])]] for v in eachslice(Ps; dims)) for dims in ((1,2), (1,3), (2,3)))
-cube_lines = [
-    Ps[1,1,:]; [Point2(NaN, NaN)];
-    Ps[1,end,:]; [Point2(NaN, NaN)];
-    Ps[end,1,:]; [Point2(NaN, NaN)];
-    Ps[end,end,:]; [Point2(NaN, NaN)];
-    Ps[1,:,1]; [Point2(NaN, NaN)];
-    Ps[1,:,end]; [Point2(NaN, NaN)];
-    Ps[end,:,1]; [Point2(NaN, NaN)];
-    Ps[end,:,end]; [Point2(NaN, NaN)];
-    Ps[:,1,1]; [Point2(NaN, NaN)];
-    Ps[:,1,end]; [Point2(NaN, NaN)];
-    Ps[:,end,1]; [Point2(NaN, NaN)];
-    Ps[:,end,end]; [Point2(NaN, NaN)];
-]
-
-text!(ax, Ps[1,1,1]; text = "min", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
-text!(ax, Ps[end,1,1]; text = "max Vdeep", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
-text!(ax, Ps[1,end,1]; text = "max VML", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
-text!(ax, Ps[1,1,end]; text = "max H", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
-text!(ax, Ps[end,end,end]; text = "max", transformation, align = (:center, :center), fontsize = 6, offset = (0, 5))
-scatter!(ax, Ps[:];
-    color = [TDval.Ē for TDval in TDvals][:],
-    colorrange = (-200, 200),
-    colormap = cgrad(:tableau_red_blue, rev = true),
-    # marker = :cross,
-    markersize = 5,
-    strokewidth = 1,
-    strokecolor = :black,
-    transformation = Transformation(ax.scene.transformation; transform_func = identity)
-)
-sc = scatter!(ax, Ps[:];
-    color = [TDval.Ē for TDval in TDvals][:],
-    colorrange = (-200, 200),
-    colormap = cgrad(:tableau_red_blue, rev = true),
-    # marker = :cross,
-    markersize = 5,
-    transformation = Transformation(ax.scene.transformation; transform_func = identity)
-)
-scatterlines!(ax, cube_lines;
-    color = (:black, 0.1),
-    markersize = 1,
-    markercolor = :black,
-    linewidth = 1,
-    transformation,
-)
-
-# Plot reference (AA age)
-scatter!(ax, Point2(xy_from_R_and_σ(1, σr));
-    color = :black,
-    transformation,
-)
-
-cb = Colorbar(fig[2, 1], sc;
-    vertical = false,
-    flipaxis = false,
-    tellheight = true,
-    # ticks = (, cbarticklabelformat.(levels)),
-    label = rich("bias (years)"),
-)
-cb.width = Relative(0.5)
-
-outputfile = joinpath(outputdir, "Taylor_diagram_3D.pdf")
+outputfile = joinpath(outputdir, "Taylor_diagram_3D_heatmaps.png")
 @info "Saving image file:\n  $(outputfile)"
 save(outputfile, fig)
+
 
