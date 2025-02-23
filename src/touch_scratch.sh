dad7d9850568e85f508b80c495cd4b34ad020176
Benoit Pasquier <briochemc@gmail.com>
2025-02-24 10:13:19 +1100

    Add command lines for touching scratch

---
+++ src/touch_scratch.sh
@@ -0,0 +1,5 @@
+# Commands to touch scratch contents to prevent archival on Gadi
+# Last ran 24 Feb 2024
+find /scratch/xv83/bp3051 -exec touch {} \;
+find /scratch/xv83/TMIP -exec touch {} \;
+find /scratch/gh0/bp3051/.julia -exec touch {} \;
