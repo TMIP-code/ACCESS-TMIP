fa8df9a4d77f72dd0d940c3829e99c1f0cc67f7d
Benoit Pasquier <briochemc@gmail.com>
2025-08-15 16:04:31 +1000

    touch scratch update

--- src/touch_scratch.sh
+++ src/touch_scratch.sh
@@ -1,5 +1,5 @@
 # Commands to touch scratch contents to prevent archival on Gadi
-# Last ran 11 Apr 2025
+# Last ran 7 Aug 2025 (may have lost time files before this date)
 find /scratch/xv83/bp3051 -exec touch {} \;
 find /scratch/xv83/TMIP -exec touch {} \;
 find /scratch/gh0/bp3051/.julia -exec touch {} \;
