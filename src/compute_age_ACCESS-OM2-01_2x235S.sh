19923f8d1bd2a74e0976203b7e12948f895e59ff
Benoit Pasquier <briochemc@gmail.com>
2025-10-10 13:28:15 +1100

    And try 2x2 N of 35S

---
+++ src/compute_age_ACCESS-OM2-01_2x235S.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#PBS -P y99
+#PBS -N age_OM2-01_2x235S
+#PBS -l ncpus=48
+#PBS -q megamem
+#PBS -l mem=2900GB
+#PBS -l jobfs=4GB
+#PBS -l walltime=48:00:00
+#PBS -l storage=scratch/gh0+scratch/y99+scratch/p66
+#PBS -l wd
+#PBS -o output/PBS/
+#PBS -j oe
+
+
+
+echo "Going into ACCESS-TMIP"
+cd ~/Projects/TMIP/ACCESS-TMIP
+
+echo "Running script"
+julia src/compute_age_ACCESS-OM2-01_2x235S.jl &> output/$PBS_JOBID.compute_age_ACCESS-OM2-01_2x235S.out
