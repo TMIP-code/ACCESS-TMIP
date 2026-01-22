855b59aba2d9771044c8494a62f134a6a7caacd1
Benoit Pasquier <briochemc@gmail.com>
2026-01-22 17:11:57 +1100

    update ACCESS-OM2-01 MOC plots across cycles for ACCESS-Hive discussion

--- src/plot_OM2-01_MOC_4cycles.jl
+++ src/plot_OM2-01_MOC_4cycles.jl
@@ -1,115 +1,115 @@
-# # qsub -I -P y99 -q express -l mem=47GB -l storage=scratch/gh0+scratch/xv83+scratch/p66 -l walltime=01:00:00 -l ncpus=12
-# # qsub -I -P y99 -l mem=47GB -l storage=scratch/gh0+scratch/xv83+scratch/p66 -l walltime=01:00:00 -l ncpus=12
+# qsub -I -P y99 -q express -l mem=47GB -l storage=scratch/gh0+scratch/xv83+scratch/p66 -l walltime=01:00:00 -l ncpus=12
+# qsub -I -P y99 -l mem=47GB -l storage=scratch/gh0+scratch/xv83+scratch/p66 -l walltime=01:00:00 -l ncpus=12
 
 
-# # TODO: Plot MOC overtutning for all files in a separate script
-# # because I keep seeing some weird stuff (different \rho ranges, AABW min locations, etc.)
-# # Probably a 3x6 panels for 3 resolutios and cycles* (4 + 2 for 0.1deg...)
+# TODO: Plot MOC overtutning for all files in a separate script
+# because I keep seeing some weird stuff (different \rho ranges, AABW min locations, etc.)
+# Probably a 3x6 panels for 3 resolutios and cycles* (4 + 2 for 0.1deg...)
 
 
-# using Pkg
-# Pkg.activate(".")
-# Pkg.instantiate()
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
 
-# using OceanTransportMatrixBuilder
-# using NetCDF
-# using YAXArrays
-# using YAXArrayBase
-# using DataFrames
-# using DimensionalData
-# # using SparseArrays
-# # using LinearAlgebra
-# using Unitful
-# using Unitful: s, yr
-# try
-#     using CairoMakie
-# catch
-#     using CairoMakie
-# end
-# using GeoMakie
-# using Interpolations
-# using OceanBasins
-# using Statistics
-# using NaNStatistics
-# using StatsBase
-# using FileIO
-# using Contour
-# using GeometryBasics
-# using GeometryOps
-# using LibGEOS
-# using DataFrames
-# # using LaTeXStrings
-# using Format
-# using KernelDensity
-# using RollingWindowArrays
-# using Dates
-# using DataStructures
+using OceanTransportMatrixBuilder
+using NetCDF
+using YAXArrays
+using YAXArrayBase
+using DataFrames
+using DimensionalData
+# using SparseArrays
+# using LinearAlgebra
+using Unitful
+using Unitful: s, yr
+try
+    using CairoMakie
+catch
+    using CairoMakie
+end
+using GeoMakie
+using Interpolations
+using OceanBasins
+using Statistics
+using NaNStatistics
+using StatsBase
+using FileIO
+using Contour
+using GeometryBasics
+using GeometryOps
+using LibGEOS
+using DataFrames
+# using LaTeXStrings
+using Format
+using KernelDensity
+using RollingWindowArrays
+using Dates
+using DataStructures
 
-# include("plotting_functions.jl")
+include("plotting_functions.jl")
 
-# #############
-# # Load data #
-# #############
-
-
-# files = [
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle1/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle2/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle3/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle4/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle5/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle6/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle1/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle2/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle3/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle4/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle5/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle6/psi_tot.nc"
-#     "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf/psi_tot.nc"
-#     "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle2/psi_tot.nc"
-#     "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle3/psi_tot.nc"
-#     "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle4/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle4_jra55v150_extension/psi_tot.nc"
-#     # "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v150_iaf_cycle1/psi_tot.nc"
-# ]
-# model_str(file) = split(file, "/")[6]
-# resolution_str(file) = split(model_str(file), "-")[3]
-# jra55version_str(file) = occursin("jra55v150", file) ? "jra55v150" : "jra55v140" # CHECK that it's v1.4 when unspecified
-# cycle_match(file) = match(r"cycle(\d+)", file)
-# cycle_str(file) = (c = cycle_match(file); isnothing(c) ? 1 : parse(Int, c.captures[1]))
-# files_df = DataFrame()
-# id_str(file) = split(file, "/")[7]
-# for file in files
-#     model = model_str(file)
-#     resolution = resolution_str(file)
-#     cycle = cycle_str(file)
-#     jra_version = jra55version_str(file)
-#     id = id_str(file)
-#     push!(files_df, (; id, file, model, resolution, cycle, jra_version))
-# end
-# @show(files_df)
-
-# psis = OrderedDict{String, Any}()
-# ρs = OrderedDict{String, Any}()
-# lats = OrderedDict{String, Any}()
-# times = OrderedDict{String, Any}()
-# meanpsis = OrderedDict{String, Any}()
+#############
+# Load data #
+#############
 
 
-# for row in eachrow(files_df)
-#     (; id, file, model, resolution, cycle, jra_version) = row
-#     @info "Loading $model $jra_version cycle $cycle overturning streamfunction"
-#     ds = open_dataset(file)
-#     psis[id] = set(readcubedata(ds.psi_tot), :time => Ti)
-#     meanpsis[id] = dropdims(mean(psis[id]; dims = Ti); dims = Ti)
-#     ρs[id] = ds.potrho
-#     lats[id] = ds.grid_yu_ocean
-#     times[id] = ds.time
-# end
+files = [
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle1/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle2/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle3/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle4/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle5/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-1/1deg_jra55_iaf_omip2_cycle6/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle1/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle2/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle3/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle4/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle5/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-025/025deg_jra55_iaf_omip2_cycle6/psi_tot.nc"
+    "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf/psi_tot.nc"
+    "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle2/psi_tot.nc"
+    "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle3/psi_tot.nc"
+    "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle4/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v140_iaf_cycle4_jra55v150_extension/psi_tot.nc"
+    # "/scratch/y99/TMIP/data/ACCESS-OM2-01/01deg_jra55v150_iaf_cycle1/psi_tot.nc"
+]
+model_str(file) = split(file, "/")[6]
+resolution_str(file) = split(model_str(file), "-")[3]
+jra55version_str(file) = occursin("jra55v150", file) ? "jra55v150" : "jra55v140" # CHECK that it's v1.4 when unspecified
+cycle_match(file) = match(r"cycle(\d+)", file)
+cycle_str(file) = (c = cycle_match(file); isnothing(c) ? 1 : parse(Int, c.captures[1]))
+files_df = DataFrame()
+id_str(file) = split(file, "/")[7]
+for file in files
+    model = model_str(file)
+    resolution = resolution_str(file)
+    cycle = cycle_str(file)
+    jra_version = jra55version_str(file)
+    id = id_str(file)
+    push!(files_df, (; id, file, model, resolution, cycle, jra_version))
+end
+@show(files_df)
 
-# ########
-# # Plot #
-# ########
+psis = OrderedDict{String, Any}()
+ρs = OrderedDict{String, Any}()
+lats = OrderedDict{String, Any}()
+times = OrderedDict{String, Any}()
+meanpsis = OrderedDict{String, Any}()
+
+
+for row in eachrow(files_df)
+    (; id, file, model, resolution, cycle, jra_version) = row
+    @info "Loading $model $jra_version cycle $cycle overturning streamfunction"
+    ds = open_dataset(file)
+    psis[id] = set(readcubedata(ds.psi_tot), :time => Ti)
+    meanpsis[id] = dropdims(mean(psis[id]; dims = Ti); dims = Ti)
+    ρs[id] = ds.potrho
+    lats[id] = ds.grid_yu_ocean
+    times[id] = ds.time
+end
+
+########
+# Plot #
+########
 
 # colors = Makie.wong_colors()
 resolutions = unique(files_df.resolution)
@@ -141,6 +141,8 @@ levels = -24:2:24
 colormap = :delta
 limits = (-90, +90, 1035, 1037.3)
 
+use_same_ρ_levels = true
+
 for (ijrow, row) in enumerate(eachrow(files_df))
 
     (; id, file, model, resolution, cycle, jra_version) = row
@@ -166,12 +168,18 @@ for (ijrow, row) in enumerate(eachrow(files_df))
 
     lat = lats[id].val
 
-    ρ = ρs[id].val
+    ρ_orig = ρs[id].val
+
+    # Only keep every second ρ level for cycle4 to match others
+    iρ_0p125 = endswith(id, "cycle4") && (use_same_ρ_levels) ? (2:2:length(ρ_orig)) : (1:length(ρ_orig))
+
+    ρ_skip = ρ_orig[iρ_0p125]
 
     # Skim some of weirdness at large densities
-    iρ = ρ .<= limits[4]
-    ρ = ρ[iρ]
-    ψ = meanpsis[id].data[:, iρ] / 1.0e9 # convert to Sv
+    iρ = ρ_skip .<= limits[4]
+    ρ = ρ_skip[iρ]
+
+    ψ = meanpsis[id].data[:, iρ_0p125[iρ]] / 1.0e9 # convert to Sv
 
     co = cfs[irow, icol] = contourf!(
         ax, lat, ρ, ψ;
@@ -190,8 +198,10 @@ for (ijrow, row) in enumerate(eachrow(files_df))
     myhidexdecorations!(ax, irow < Nrows)
     myhideydecorations!(ax, icol > 1)
 
+    cycle_extra_str = cycle > 4 ? superscript("*") : ""
+    cycle_extra_str_2 = " (ρ=$ρ_skip)"
     (icol == 1) && Label(fig[irow, 0], "$(resolution°[resolution]) ACCESS-OM2", tellheight = false, rotation = π / 2, font = :bold)
-    (irow == 1) && Label(fig[0, icol], rich("Cycle $cycle", cycle > 4 ? superscript("*") : ""), tellwidth = false, font = :bold)
+    (irow == 1) && Label(fig[0, icol], rich("Cycle $cycle", cycle_extra_str, cycle_extra_str_2), tellwidth = false, font = :bold)
 
     # add a red box over those 0.1deg runs that are not part of the main 6 cycles...
     # (resolution == "01") && (icol > 4) && Box(fig[irow, icol]; color = (Pattern('/'), 0.3), strokecolor = :transparent)
@@ -215,7 +225,7 @@ cb.width = Relative(1 / 4)
 
 fig
 
-imagefile = "/scratch/y99/TMIP/data/OM2-01_MOC_4cycles.png"
+imagefile = "/scratch/y99/TMIP/data/OM2-01_MOC_4cycles$(use_same_ρ_levels ? "_same_ρ_levels" : "").png"
 save(imagefile, fig)
 @info "Saved AABW timeseries plot to $imagefile"
 # imagefile = "/scratch/y99/TMIP/data/OM2-01_MOC_4cycles.pdf"
